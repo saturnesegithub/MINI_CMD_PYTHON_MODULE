4ce36c5d605b31720fdeef108919e470547e3946
saturnesegithub <134809658+saturnesegithub@users.noreply.github.com>
2023-05-28 11:30:02 +0100

    mini_cmd.py

--- mini_cmd.py
+++ mini_cmd.py
@@ -1,61 +1,119 @@
+import time
+
+import sys
+
+import os
+
+import subprocess
+
+import platform
+
+###########CODE##############
+
 def addusers(user):
+
     userlist = []
+
     userlist.append(user)
+
+    return userlist
+
 def choice(prompt):
+
     prompt = ""
+
     prompt = input()
+
 def cipher(prompt):
+
     binary_text = ''.join(format(ord(char), '08b') for char in prompt)
+
     return binary_text
+
 def date():
-    import time
+
     calendar = time.strftime("%Y-%m-%d")
+
     print(calendar)
+
 def exit():
-    import sys
+
     sys.exit()
+
 def hostname():
-    import sys
+
     print(sys.platform)
+
 def logoff():
-    import sys
+
     sys.exit
+
 def msg(prompt = ""):
+
     prompt = input()
+
     print("msg: " + prompt)
+
 def msinfo32():
-    import sys
+
     print(sys.version)
+
 def now():
-    import time
+
     print(time.localtime())
+
 def pause(paused):
-    import time
+
     time.sleep(paused)
+
 def rcp(listname,copylist):
-    copylist.extend(listname)
+
+    return copylist.extend(listname)
+
 def sleep(sleep_time):
-    import time
+
     time.sleep(sleep_time)
-def t
-    import sys
+
+def tree():
+
     print(sys.path)
+
 def xcopy():
-    import sys
+
     xcopy_data = [sys.path]
+
 def run_calc():
+
     evaluate = input()
-    print(int(eval(evaluate)))
-def run_cmd():
-    print("CMDCEPTION!")
+
+    print(int(literal_eval(evaluate)))
+
+def run_cmd(input):
+
+    subprocess.run(input)
+
 def delt(name):
-    import os
+
     os.remove(name)
+
 def find(listname,listelement):
+
     if listelement in listname:
+
         print("True")
+
     else:
+
         print("False")
+
 def systeminfo_plus():
-    import platform
+
     print(platform.platform())
+
+def dir():
+
+    return os.listdir()
+
+    file_list = dir()
+
+    return file_list
